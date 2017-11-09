a357612569af7d74f509b656ce0aa6284f64e0ae
Michel Weststrate <mweststrate@gmail.com>
2017-11-09 13:25:17 +0100

    lesson 13: identifiers and references

--- src/components/WishListView.js
+++ src/components/WishListView.js
@@ -4,11 +4,15 @@ import { observer } from "mobx-react"
 import WishListItemView from "./WishListItemView"
 import WishListItemEntry from "./WishListItemEntry"
 
-const WishListView = ({ wishList }) => (
+const WishListView = ({ wishList, readonly }) => (
     <div className="list">
-        <ul>{wishList.items.map((item, idx) => <WishListItemView key={idx} item={item} />)}</ul>
+        <ul>
+            {wishList.items.map((item, idx) => (
+                <WishListItemView key={idx} item={item} readonly={readonly} />
+            ))}
+        </ul>
         Total: {wishList.totalPrice} €
-        <WishListItemEntry wishList={wishList} />
+        {!readonly && <WishListItemEntry wishList={wishList} />}
     </div>
 )
 
--- src/models/Group.js
+++ src/models/Group.js
@@ -1,13 +1,14 @@
-import { types, flow } from "mobx-state-tree"
+import { types, flow, getParent } from "mobx-state-tree"
 
 import { WishList } from "./WishList"
 
 const User = types
     .model({
-        id: types.string,
+        id: types.identifier(),
         name: types.string,
         gender: types.enumeration("gender", ["m", "f"]),
-        wishList: types.optional(WishList, {})
+        wishList: types.optional(WishList, {}),
+        recipient: types.maybe(types.reference(types.late(() => User)))
     })
     .actions(self => ({
         getSuggestions: flow(function* getSuggestions() {
@@ -16,6 +17,40 @@ const User = types
         })
     }))
 
-export const Group = types.model({
-    users: types.map(User)
-})
+export const Group = types
+    .model({
+        users: types.map(User)
+    })
+    .actions(self => ({
+        drawLots() {
+            const allUsers = self.users.values()
+
+            // not enough users, bail out
+            if (allUsers.length <= 1) return
+
+            // not assigned lots
+            let remaining = allUsers.slice()
+
+            allUsers.forEach(user => {
+                // edge case: the only person without recipient
+                // is the same as the only remaining lot
+                // swap lot's with some random other person
+                if (remaining.length === 1 && remaining[0] === user) {
+                    const swapWith = allUsers[Math.floor(Math.random() * (allUsers.length - 1))]
+                    user.recipients = swapWith.recipient
+                    swapWith.recipient = self
+                } else
+                    while (!user.recipient) {
+                        // Pick random lot from remaing list
+                        let recipientIdx = Math.floor(Math.random() * remaining.length)
+
+                        // If it is not the current user, assign it as recipient
+                        // and remove the lot
+                        if (remaining[recipientIdx] !== user) {
+                            user.recipient = remaining[recipientIdx]
+                            remaining.splice(recipientIdx, 1)
+                        }
+                    }
+            })
+        }
+    }))
