7c1ffbf5c5ae6f9864b319f4bf57f22c8be87e61
Michel Weststrate <mweststrate@gmail.com>
2017-11-17 20:59:05 +0100

    lesson 2: actions

--- src/models/WishList.js
+++ src/models/WishList.js
@@ -1,11 +1,29 @@
 import { types } from "mobx-state-tree"
 
-export const WishListItem = types.model({
-    name: types.string,
-    price: types.number,
-    image: ""
-})
+export const WishListItem = types
+    .model({
+        name: types.string,
+        price: types.number,
+        image: ""
+    })
+    .actions(self => ({
+        changeName(newName) {
+            self.name = newName
+        },
+        changePrice(newPrice) {
+            self.price = newPrice
+        },
+        changeImage(newImage) {
+            self.image = newImage
+        }
+    }))
 
-export const WishList = types.model({
-    items: types.optional(types.array(WishListItem), [])
-})
+export const WishList = types
+    .model({
+        items: types.optional(types.array(WishListItem), [])
+    })
+    .actions(self => ({
+        add(item) {
+            self.items.push(item)
+        }
+    }))
