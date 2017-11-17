948f9760367cc1359b8e044911e197bc78ca640d
Michel Weststrate <mweststrate@gmail.com>
2017-11-17 21:21:38 +0100

    lesson 6: editing wishlist items

--- src/components/WishListItemView.js
+++ src/components/WishListItemView.js
@@ -1,12 +1,59 @@
-import React from "react"
+import React, { Component } from "react"
 import { observer } from "mobx-react"
+import { clone, getSnapshot, applySnapshot } from "mobx-state-tree"
 
-const WishListItemView = ({ item }) => (
-    <li className="item">
-        {item.image && <img src={item.image} />}
-        <h3>{item.name}</h3>
-        <span>{item.price}</span>
-    </li>
-)
+import WishListItemEdit from "./WishListItemEdit"
+
+class WishListItemView extends Component {
+    constructor() {
+        super()
+        this.state = { isEditing: false }
+    }
+
+    render() {
+        const { item } = this.props
+        return this.state.isEditing ? (
+            this.renderEditable()
+        ) : (
+            <li className="item">
+                {item.image && <img src={item.image} />}
+                <h3>{item.name}</h3>
+                <span>{item.price}</span>
+                <span>
+                    <button onClick={this.onToggleEdit}>✏</button>
+                </span>
+            </li>
+        )
+    }
+
+    renderEditable() {
+        return (
+            <li className="item">
+                <WishListItemEdit item={this.state.clone} />
+                <button onClick={this.onSaveEdit}>💾</button>
+                <button onClick={this.onCancelEdit}>❎</button>
+            </li>
+        )
+    }
+
+    onToggleEdit = () => {
+        this.setState({
+            isEditing: true,
+            clone: clone(this.props.item)
+        })
+    }
+
+    onCancelEdit = () => {
+        this.setState({ isEditing: false })
+    }
+
+    onSaveEdit = () => {
+        applySnapshot(this.props.item, getSnapshot(this.state.clone))
+        this.setState({
+            isEditing: false,
+            clone: null
+        })
+    }
+}
 
 export default observer(WishListItemView)
