bda0250b2ede089778bef148299854c574ad1550
Michel Weststrate <mweststrate@gmail.com>
2017-11-17 21:04:42 +0100

    lesson 4: derived information

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,4 +1,5 @@
 import { getSnapshot, onSnapshot, onPatch } from "mobx-state-tree"
+import { reaction } from "mobx"
 import { WishList, WishListItem } from "./WishList"
 
 it("can create a instance of a model", () => {
@@ -64,4 +65,32 @@ it("can add new items", () => {
     expect(getSnapshot(list)).toMatchSnapshot()
 
     expect(states).toMatchSnapshot()
+})
+
+it("can calculate the total price of a wishlist", () => {
+    const list = WishList.create({
+        items: [
+            {
+                name: "Machine Gun Preacher",
+                price: 7.35,
+                image: "https://images-na.ssl-images-amazon.com/images/I/91AFFK9fwkL._SY445_.jpg"
+            },
+            {
+                name: "LEGO Mindstorms EV3",
+                price: 349.95,
+                image: "https://images-na.ssl-images-amazon.com/images/I/71CpQw%2BufNL._SL1000_.jpg"
+            }
+        ]
+    })
+
+    expect(list.totalPrice).toBe(357.3)
+
+    let changed = 0
+    reaction(() => list.totalPrice, () => changed++)
+
+    expect(changed).toBe(0)
+    console.log(list.totalPrice)
+    list.items[0].changeName("Test")
+    expect(changed).toBe(0)
+    list.items[0].changePrice(10)
 })
