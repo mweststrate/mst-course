0866f7e1f81307cc6b5d625b79612ed0da40c743
Michel Weststrate <mweststrate@gmail.com>
2017-11-17 21:03:43 +0100

    lesson 3: testing with snapshots and patches

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -27,17 +27,41 @@ it("can create a wishlist", () => {
     expect(list.items[0].price).toBe(28.73)
 })
 
+it("can add new items - 2", () => {
+    const list = WishList.create()
+    const patches = []
+    onPatch(list, patch => {
+        patches.push(patch)
+    })
+
+    list.add({
+        name: "Chesterton",
+        price: 10
+    })
+
+    list.items[0].changeName("Book of G.K. Chesterton")
+
+    expect(patches).toMatchSnapshot()
+})
+
 it("can add new items", () => {
     const list = WishList.create()
-    list.add(
-        WishListItem.create({
-            name: "Chesterton",
-            price: 10
-        })
-    )
+    const states = []
+    onSnapshot(list, snapshot => {
+        states.push(snapshot)
+    })
+
+    list.add({
+        name: "Chesterton",
+        price: 10
+    })
 
     expect(list.items.length).toBe(1)
     expect(list.items[0].name).toBe("Chesterton")
     list.items[0].changeName("Book of G.K. Chesterton")
     expect(list.items[0].name).toBe("Book of G.K. Chesterton")
+
+    expect(getSnapshot(list)).toMatchSnapshot()
+
+    expect(states).toMatchSnapshot()
 })
