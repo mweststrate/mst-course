399494684b0c0b4c1b55ca16ada4007dc1bc838f
Michel Weststrate <mweststrate@gmail.com>
2017-11-17 20:59:05 +0100

    lesson 2: actions

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,3 +1,4 @@
+import { getSnapshot, onSnapshot, onPatch } from "mobx-state-tree"
 import { WishList, WishListItem } from "./WishList"
 
 it("can create a instance of a model", () => {
@@ -8,6 +9,8 @@ it("can create a instance of a model", () => {
 
     expect(item.price).toBe(28.73)
     expect(item.image).toBe("")
+    item.changeName("Narnia")
+    expect(item.name).toBe("Narnia")
 })
 
 it("can create a wishlist", () => {
@@ -22,4 +25,19 @@ it("can create a wishlist", () => {
 
     expect(list.items.length).toBe(1)
     expect(list.items[0].price).toBe(28.73)
+})
+
+it("can add new items", () => {
+    const list = WishList.create()
+    list.add(
+        WishListItem.create({
+            name: "Chesterton",
+            price: 10
+        })
+    )
+
+    expect(list.items.length).toBe(1)
+    expect(list.items[0].name).toBe("Chesterton")
+    list.items[0].changeName("Book of G.K. Chesterton")
+    expect(list.items[0].name).toBe("Book of G.K. Chesterton")
 })
