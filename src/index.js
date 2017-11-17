cd723d6cc99ac6975fdf218b96cd6ca9e4a31ff8
Michel Weststrate <mweststrate@gmail.com>
2017-11-17 21:14:46 +0100

    lesson 5: rendering models

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./assets/index.css"
+import App from "./components/App"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { WishList } from "./models/WishList"
+
+const wishList = WishList.create({
+    items: [
+        {
+            name: "LEGO Mindstorms EV3",
+            price: 349.95,
+            image: "https://images-na.ssl-images-amazon.com/images/I/71CpQw%2BufNL._SL1000_.jpg"
+        },
+        {
+            name: "Miracles - C.S. Lewis",
+            price: 12.91,
+            image:
+                "https://images-na.ssl-images-amazon.com/images/I/51a7xaMpneL._SX329_BO1,204,203,200_.jpg"
+        }
+    ]
+})
+
+ReactDOM.render(<App wishList={wishList} />, document.getElementById("root"))
+
+setInterval(() => {
+    wishList.items[0].changePrice(wishList.items[0].price + 1)
+}, 1000)
